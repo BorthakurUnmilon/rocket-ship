841be529ccf06d3b904aae8e942a2ead92665a55
Unmilon Borthakur <unmilon.7122@gmail.com>
2021-03-27 21:52:58 +0530

    new commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+gsap.registerPlugin(MotionPathPlugin);
+gsap.to("#rect", {
+    motionPath: {
+        path: "#path",
+        align: "#path",
+        alignOrigin: [0.5, 0.5],
+        autoRotate: true
+    },
+    transformOrigin: "50% 50%",
+    duration: 5,
+    ease: "power1.inOut"
+})
